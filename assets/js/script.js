84566c24c6f663deb46c6dc6148538d8373f4966
Fival <thefivalco@gmail.com>
2020-12-14 11:09:53 -0600

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,28 @@ var time = moment();
 var displayMoment = document.getElementById("currentDay");
 displayMoment.innerHTML = time.format("M-D-YYYY, h:mma");
 
-//var time = getHours();
+var nowTime = moment().hours()
 
 
-
-
-$(".col-1").each(function() {
-       if (time > 12) {
-       $("#row-1").addClass("bg-secondary")
-}
-        else {
-        if (time < 1)
-    $("#row-1").addClass("bg-warning")
-}
-    
+$(".hour").each(function() {
+      if (parseInt($(this).data("pineapple")) < nowTime) {
+            $(this).next().addClass("bg-secondary")
+      }
+      if (parseInt($(this).data("pineapple")) == nowTime) {
+            $(this).next().addClass("bg-success")
+      }
+      if (parseInt($(this).data("pineapple")) > nowTime) {
+            $(this).next().addClass("bg-warning")
+      }
 })
 
-
-
-
-
-
-
-//$('hour').each(function(index, value){
-    // now we're in a loop!
-  // You can also use jQuery to find the ID of the current element
-  // You'll need the id of the current element to use in your if statement; that's how we'll decide what color the time block is!
-  //});
+$("#row-1").click (function() {
+      //var text = $(this)
+      var textInput = $("<textarea>")
+      $(this).replaceWith(textInput)
+      textInput.trigger("focus")
+      //.addClass("form-control")
+      //.val(text)
+      //.text()
+      //.trim()
+    })
